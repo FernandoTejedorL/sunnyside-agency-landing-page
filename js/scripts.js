e0adf1a93476228f807d19f00dfcaff19cad4bd4
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-14 01:46:25 +0100

    completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 const rootStyles = document.documentElement.style;
+const hamburgerElement = document.getElementById('hamburger');
+const modalElement = document.getElementById('modal');
+
+const toModal = () => {
+  if (modalElement.classList.contains('modal')) {
+    modalElement.classList.replace('modal', 'hide-modal');
+  } else {
+    modalElement.classList.replace('hide-modal', 'modal');
+  }
+};
+hamburgerElement.addEventListener('click', toModal);
