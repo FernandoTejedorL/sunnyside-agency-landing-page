1150cad1ac5a8cc3393853a773a825da38a3d6df
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-15 14:38:32 +0100

    completed and corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 const rootStyles = document.documentElement.style;
 const hamburgerElement = document.getElementById('hamburger');
-const modalElement = document.getElementById('modal');
+const modalElement = document.getElementById('menu');
 
 const toModal = () => {
-  if (modalElement.classList.contains('modal')) {
-    modalElement.classList.replace('modal', 'hide-modal');
+  if (modalElement.classList.contains('menu')) {
+    modalElement.classList.replace('menu', 'hide-menu');
   } else {
-    modalElement.classList.replace('hide-modal', 'modal');
+    modalElement.classList.replace('hide-menu', 'menu');
   }
 };
 hamburgerElement.addEventListener('click', toModal);
